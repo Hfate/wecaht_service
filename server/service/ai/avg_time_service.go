84e6fb7f8897dfe5b750547c152b865d2655a46c
changqi.huang@shopee.com <changqi.huang@shopee.com>
2024-05-01 00:54:50 +0800

    更新

--- server/service/ai/avg_time_service.go
+++ server/service/ai/avg_time_service.go
@@ -4,6 +4,7 @@ import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/ai"
 	"github.com/flipped-aurora/gin-vue-admin/server/utils"
+	"github.com/spf13/cast"
 )
 
 type AvgTimeService struct {
@@ -12,9 +13,9 @@ type AvgTimeService struct {
 var AvgTimeServiceApp = new(AvgTimeService)
 
 func (*AvgTimeService) FindAvgTime() int64 {
-	var avgTime int64
+	var avgTime float64
 	global.GVA_DB.Model(&ai.AvgTime{}).Where("1=1").Select("avg(span_time)").Find(&avgTime)
-	return avgTime
+	return cast.ToInt64(avgTime)
 }
 
 func (*AvgTimeService) UpdateAvgTime(spanTime int64) {
